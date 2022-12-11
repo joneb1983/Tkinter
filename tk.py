b51858389f4dff072cd68bbc009329c458b55186
Evgeny Borozdin <joneb1983@gmail.com>
2022-12-11 15:43:26 +0300

    Добавление icon.png

--- tk.py
+++ tk.py
@@ -2,4 +2,10 @@ import tkinter as tk
 
 win = tk.Tk()
 win.title("Auto create letter outlook (ACLO)")
+win.geometry("500x400+100+200")
+win.resizable(False, False)
+photo = tk.PhotoImage(file="icon.png")
+win.iconphoto(False, photo)
+
+
 win.mainloop()
